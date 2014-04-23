274241803c672123f37c8b4b7993713830312faa
rafagonc <rafagonc77@yahoo.com.br>
2014-04-23 14:27:27 -0300

    Initial Commit

---
+++ MiniChallange2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiniChallange2
+//
+//  Created by Rafael Gonçalves on 4/23/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
